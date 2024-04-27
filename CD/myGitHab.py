6cb1d33cc50069914411b9c7e158091226d75b91
LLI0P0X <laketc@list.ru>
2024-04-27 19:54:04 +0300

    fix imports

--- CD/myGitHab.py
+++ CD/myGitHab.py
@@ -1,4 +1,9 @@
 import subprocess
+import os
+import sys
+
+project_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_directory)
 import config
 
 
--- CD/myService.py
+++ CD/myService.py
@@ -4,7 +4,6 @@ import sys
 
 project_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(project_directory)
-
 import config
 
 
