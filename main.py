74a34634a82e021811a36115f58fd365f6c73548
LLI0P0X <laketc@list.ru>
2024-05-02 09:27:23 +0300

    hot fix startup message

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from handlers import router
 
 async def main():
     bot = Bot(token=config.BOT_TOKEN)
+    for ids in config.TOP_ADMINS:
+        await bot.send_message(ids, strConfig.startMsgForAdm)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
-    for ids in config.TOP_ADMINS:
-        await bot.send_message(ids, strConfig.startMsgForAdm)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
