a606c87025cca035614254e9be0953d33a2486f2
LLI0P0X <laketc@list.ru>
2024-11-06 17:01:28 +0300

    hotfix

--- pgSQL/sqlFuncs.py
+++ pgSQL/sqlFuncs.py
@@ -8,22 +8,26 @@ import os
 
 import config
 
-
 import tables
 from tables import engine, Users
+
 
 async def add_user(**kwargs):
     async with engine.begin() as conn:
         await conn.execute(
             insert(Users).values(**kwargs))
 
+
 async def select_users():
     async with engine.begin() as conn:
         result = await conn.execute(select(Users))
         return result.fetchall()
+
+
 async def main():
     await add_user(tg_id=123)
     print(await select_users())
+
 
 if __name__ == "__main__":
     asyncio.run(main())
